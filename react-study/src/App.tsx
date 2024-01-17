dc54dcf2aebb3c7ddd9a5c400a7d26889ca667c2
xuanlvzeng <xuanlvzeng@gmail.com>
2024-01-17 20:40:32 +0800

    复习：MVVM和vue的响应式原理

--- react-study/src/App.tsx
+++ react-study/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import useStateDemo from './hooks/useStateDemo';
 import useEffectDemo from './hooks/useEffectDemo';
@@ -6,18 +7,38 @@ import Example2 from './components/Example2';
 import MemoDemo from './components/MemoDemo';
 
 function App() {
-  const { value, clickHandler } = useStateDemo();
-  const { value: effectValue, clickHandler: effectClickHandler } = useEffectDemo();
+  // const { value, clickHandler } = useStateDemo();
+  // const { value: effectValue, clickHandler: effectClickHandler } = useEffectDemo();
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setTimeout(() => {
+      setCount(count + 1)
+    }, 0)
+  }
+  // console.log('1', count);
 
+  // useEffect(() => {
+  //   setCount(count => count + 1);
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log('2', count);
+  //   setTimeout(() => {
+  //     console.log('3', count);
+  //   }, 100);
+  // }, []);
+  console.log(count);
+  
   return (
     <div className='App'>
+      <button onClick={increment}>click me</button>
       {/* <button onClick={clickHandler}>点击 {value}</button>
       <div>
         value: {effectValue} <button onClick={effectClickHandler}>点击</button>
       </div>
       <Example />
       <Example2 /> */}
-      <MemoDemo />
+      {/* <MemoDemo /> */}
     </div>
   );
 }
